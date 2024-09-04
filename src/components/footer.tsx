3395dee6ee5be66bda565247e4a5305f70c7cd27
Serbe <serbenv@gmail.com>
2024-09-04 15:40:02 +0300

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,32 +1,153 @@
 const Footer = () => {
   return (
-    <header className="sticky bottom-0 bg-black text-slate-300">
-      <div className="mx-auto flex max-w-5xl py-2">
-        <div className="pr-4 text-6xl">GAA
-        </div>
-        <div className="pt-1 text-xs tracking-[0.4em]" >
-          <div>Global</div>
-          <div>Automotive</div>
-          <div>Association</div>
-        </div>
-        <div className="flex w-full text-center text-sm">
-          <div className="w-1/4">Privacy Policy</div>
-          <div className="w-1/4">Term & Conditions</div>
-          <div className="w-1/4">About Us</div>
-          <div className="w-1/4">Contacts</div>
-        </div>
-      </div>
+    <header className="sticky bottom-0 bg-teal-900 text-slate-300">
       <div className="mx-auto flex max-w-5xl py-2 text-sm">
-        <div>© 2024 All Rights Reserved</div>
-        <div className="flex w-full">
-          <div className="w-full"></div>
-          <div className="px-2">Instagramm</div>
-          <div className="px-2">Facebook</div>
-          <div className="px-2">Twitter</div>
+        <a href="#" className="w-1/3">
+          Privacy policy
+        </a>
+        <div className="w-1/3 text-center">© 2024 All Rights Reserved</div>
+        <div className="w-1/3">
+          <div className="mx-auto flex justify-end">
+            <a href="https://discord.com/invite/gaa" title="Discord">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#8c9eff"
+                  d="M40,12c0,0-4.585-3.588-10-4l-0.488,0.976C34.408,10.174,36.654,11.891,39,14c-4.045-2.065-8.039-4-15-4s-10.955,1.935-15,4c2.346-2.109,5.018-4.015,9.488-5.024L18,8c-5.681,0.537-10,4-10,4s-5.121,7.425-6,22c5.162,5.953,13,6,13,6l1.639-2.185C13.857,36.848,10.715,35.121,8,32c3.238,2.45,8.125,5,16,5s12.762-2.55,16-5c-2.715,3.121-5.857,4.848-8.639,5.815L33,40c0,0,7.838-0.047,13-6C45.121,19.425,40,12,40,12z M17.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C21,28.209,19.433,30,17.5,30z M30.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C34,28.209,32.433,30,30.5,30z"
+                ></path>
+              </svg>
+            </a>
+            <a href="https://t.me/GlobalAutomotiveAssociation" title="Channel">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#29b6f6"
+                  d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"
+                ></path>
+                <path
+                  fill="#b0bec5"
+                  d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"
+                ></path>
+                <path
+                  fill="#cfd8dc"
+                  d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"
+                ></path>
+              </svg>
+            </a>
+            <a href="https://t.me/GAAChat" title="Community chat">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#29b6f6"
+                  d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"
+                ></path>
+                <path
+                  fill="#b0bec5"
+                  d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"
+                ></path>
+                <path
+                  fill="#cfd8dc"
+                  d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"
+                ></path>
+              </svg>
+            </a>
+            <a href="https://x.com/RaceLads" title="Twitter">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0,0,256,256"
+              >
+                <g
+                  fill="none"
+                  fill-rule="none"
+                  stroke="none"
+                  stroke-width="1"
+                  stroke-linecap="butt"
+                  stroke-linejoin="miter"
+                  stroke-miterlimit="10"
+                  stroke-dasharray=""
+                  stroke-dashoffset="0"
+                  font-family="none"
+                  font-weight="none"
+                  font-size="none"
+                  text-anchor="none"
+                  className="mix-blend-normal"
+                >
+                  <g transform="scale(5.33333,5.33333)">
+                    <path
+                      d="M38,42h-28c-2.209,0 -4,-1.791 -4,-4v-28c0,-2.209 1.791,-4 4,-4h28c2.209,0 4,1.791 4,4v28c0,2.209 -1.791,4 -4,4z"
+                      fill="#1798d2"
+                      fill-rule="evenodd"
+                    ></path>
+                    <path
+                      d="M34.257,34h-6.437l-13.991,-20h6.437zM28.587,32.304h2.563l-11.651,-16.608h-2.563z"
+                      fill="#ffffff"
+                      fill-rule="nonzero"
+                    ></path>
+                    <path
+                      d="M15.866,34l7.203,-8.344l-0.942,-1.249l-8.304,9.593z"
+                      fill="#ffffff"
+                      fill-rule="nonzero"
+                    ></path>
+                    <path
+                      d="M24.45,21.721l0.905,1.289l7.781,-9.01h-2z"
+                      fill="#ffffff"
+                      fill-rule="nonzero"
+                    ></path>
+                  </g>
+                </g>
+              </svg>
+            </a>
+            <a href="#" title="Youtube">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#FF3D00"
+                  d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"
+                ></path>
+                <path fill="#FFF" d="M20 31L20 17 32 24z"></path>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
