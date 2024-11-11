3eb21aa5efb8dbd2b5ba27df392ba6566fd43ac5
Serbe <serbenv@gmail.com>
2024-11-11 10:50:14 +0300

    стелал липкий блок

--- src/app/rumintcars/page.tsx
+++ src/app/rumintcars/page.tsx
@@ -18,124 +18,128 @@ export default function Page() {
       </div>
       <section className="flex flex-wrap rounded-3xl border-cyan-700 bg-p1-white p-2 md:flex-nowrap md:border-4 md:shadow-lg">
         <div className="grid grid-cols-1 md:w-max md:grid-cols-2 md:gap-4">
-          <div className="p-2">
+          <div className="">
             <div className="text-center md:text-2xl lg:text-2xl">
               <div className="">
-                <div className="flex flex-wrap px-2 pt-2 md:fixed md:w-1/4 md:flex-nowrap">
+                <div className="flex flex-wrap px-2 pt-2 md:flex-nowrap">
                   <div className="">
                     <div className="text-center text-base font-semibold text-p1-darkgreen md:text-xl">
                       <p>Присоединяйтесь к нам</p>
                       <p>Создай NFT спортивного/раритетного автомобиля</p>
                     </div>
 
-                    <div className="grid-1 grid flex-wrap-reverse gap-2 md:w-full md:grid-cols-3">
-                      <div className="place-items-center p-2">
-                        <Image
-                          className="relative mb-4 inline-block w-max rounded-3xl border-2 border-cyan-700 shadow-lg"
-                          width={1000}
-                          height={1000}
-                          src="/Cars/Car.png"
-                          alt="Car"
-                        />
-                        <button className="w-full rounded-3xl border-2 border-p1-darkgreen bg-p2-white2 p-8 text-center text-base font-normal text-p1-darkgreen shadow-lg hover:bg-cyan-700 hover:text-p1-white md:text-xl">
-                          Сгенерировать изображение
-                        </button>
-                      </div>
-                      <div className="m-1 place-items-center p-2">
-                        <label
-                          htmlFor="dropzone-file"
-                          className="flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-gray-300 bg-gray-50 shadow-lg hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-800"
-                        >
-                          <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                            <svg
-                              className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 20 16"
-                            >
-                              <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                              />
-                            </svg>
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                              <span className="font-semibold">
-                                Изображение реального авто в картинке
-                              </span>
-                            </p>
+                    <div className="z-10 rounded-3xl border-2 bg-p3-blue py-5 md:sticky md:top-20">
+                      <div className="grid-1 grid flex-wrap-reverse gap-2 md:w-full md:grid-cols-3">
+                        <div className="place-items-center p-2">
+                          <Image
+                            className="relative mb-4 inline-block w-max rounded-3xl border-2 border-cyan-700 shadow-lg"
+                            width={1000}
+                            height={1000}
+                            src="/Cars/Car.png"
+                            alt="Car"
+                          />
+                          <button className="w-full rounded-3xl border-2 border-p1-darkgreen bg-p2-white2 p-8 text-center text-base font-normal text-p1-darkgreen shadow-lg hover:bg-cyan-700 hover:text-p1-white md:px-1 md:py-2 md:text-xs">
+                            Сгенерировать изображение
+                          </button>
+                        </div>
+                        <div className="m-1 place-items-center p-2">
+                          <label
+                            htmlFor="dropzone-file"
+                            className="flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-gray-300 bg-gray-50 shadow-lg hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-800"
+                          >
+                            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                              <svg
+                                className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 16"
+                              >
+                                <path
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                />
+                              </svg>
+                              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span className="font-semibold">
+                                  Изображение реального авто в картинке
+                                </span>
+                              </p>
 
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                              <span className="font-semibold">Click to upload</span> or drag and
-                              drop
-                            </p>
-                            <p className="text-xs text-gray-500 dark:text-gray-400">
-                              SVG, PNG, JPG or GIF (MAX. 800x400px)
-                            </p>
-                          </div>
-                          <input id="dropzone-file" type="file" className="hidden" />
-                        </label>
-                      </div>
-                      <div className="m-1 place-items-center p-2">
-                        <label
-                          htmlFor="dropzone-file"
-                          className="flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-gray-300 bg-gray-50 shadow-lg hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-800"
-                        >
-                          <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                            <svg
-                              className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 20 16"
-                            >
-                              <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                              />
-                            </svg>
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                              <span className="font-semibold">Фото автомобиля</span>
-                            </p>
+                              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span className="font-semibold">Click to upload</span> or drag and
+                                drop
+                              </p>
+                              <p className="text-xs text-gray-500 dark:text-gray-400">
+                                SVG, PNG, JPG or GIF (MAX. 800x400px)
+                              </p>
+                            </div>
+                            <input id="dropzone-file" type="file" className="hidden" />
+                          </label>
+                        </div>
+                        <div className="m-1 place-items-center p-2">
+                          <label
+                            htmlFor="dropzone-file"
+                            className="flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-gray-300 bg-gray-50 shadow-lg hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-800"
+                          >
+                            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                              <svg
+                                className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 16"
+                              >
+                                <path
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                />
+                              </svg>
+                              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span className="font-semibold">Фото автомобиля</span>
+                              </p>
 
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                              <span className="font-semibold">Click to upload</span> or drag and
-                              drop
-                            </p>
-                            <p className="text-xs text-gray-500 dark:text-gray-400">
-                              SVG, PNG, JPG or GIF (MAX. 800x400px)
-                            </p>
-                          </div>
-                          <input id="dropzone-file" type="file" className="hidden" />
-                        </label>
+                              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span className="font-semibold">Click to upload</span> or drag and
+                                drop
+                              </p>
+                              <p className="text-xs text-gray-500 dark:text-gray-400">
+                                SVG, PNG, JPG or GIF (MAX. 800x400px)
+                              </p>
+                            </div>
+                            <input id="dropzone-file" type="file" className="hidden" />
+                          </label>
+                        </div>
+                      </div>
+
+                      <div className="md:w-full">
+                        <div>
+                          <label
+                            htmlFor="default-range"
+                            className="mb-2 mt-5 block text-2xl font-medium text-gray-900 dark:text-white"
+                          >
+                            {value}
+                          </label>
+                          <input
+                            id="default-range"
+                            type="range"
+                            min="1"
+                            max="100"
+                            value={value}
+                            onChange={(e) => setValue(e.target.value)}
+                            className="h-2 w-3/5 cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
+                          />
+                        </div>
                       </div>
                     </div>
 
                     <div className="md:w-full">
-                      <div>
-                        <label
-                          htmlFor="default-range"
-                          className="mb-2 mt-5 block text-2xl font-medium text-gray-900 dark:text-white"
-                        >
-                          {value}
-                        </label>
-                        <input
-                          id="default-range"
-                          type="range"
-                          min="1"
-                          max="100"
-                          value={value}
-                          onChange={(e) => setValue(e.target.value)}
-                          className="h-2 w-3/5 cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
-                        />
-                      </div>
-
                       <p className="p-2 pt-8 text-base font-normal text-p1-darkgreen md:text-2xl">
                         Максимальное количество: 100 Cars NFT за транзакцию
                       </p>
