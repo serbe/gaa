336f4e1cdcf6b449c9ff2f83dcc5a18a32aadf0c
Serbe <serbenv@gmail.com>
2024-11-14 17:10:35 +0300

    забыл добавить скролл в начало по клику

--- src/components/tButton.tsx
+++ src/components/tButton.tsx
@@ -22,8 +22,15 @@ export const TButton = () => {
       type="button"
       data-twe-ripple-init
       data-twe-ripple-color="light"
-      className={`!fixed bottom-16 ${visible ? 'end-6' : '-end-12'} z-15 overflow-hidden rounded-full bg-p2-orange p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition-all duration-1000 hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg`}
+      className={`!fixed bottom-16 ${visible ? 'end-6' : '-end-12'} z-15 overflow-hidden rounded-full bg-p2-orange p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition-all duration-1000 hover:bg-p2-red hover:shadow-lg focus:bg-p2-red focus:shadow-lg focus:outline-none focus:ring-0 active:bg-p2-red active:shadow-lg`}
       id="btn-back-to-top"
+      onClick={() => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        });
+      }}
     >
       <span className="[&>svg]:w-4">
         <svg
