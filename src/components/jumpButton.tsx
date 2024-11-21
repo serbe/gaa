0ed9247025112662b25001cc6d10160ddc69fcb8
Serbe <serbenv@gmail.com>
2024-11-21 12:34:15 +0300

    новая реализация переходов

--- src/components/jumpButton.tsx
+++ src/components/jumpButton.tsx
@@ -1,24 +1,18 @@
-'use client';
-
-import { useState } from 'react';
-
 export const JumpButton = ({ points }: { points: string[] }) => {
-  const [position, setPosition] = useState(1);
-
-  const nextPosition = () => {
-    const next = position + 1 < points.length ? position + 1 : 0;
-    setPosition(next);
-  };
-
   const jump = () => {
-    const element = document.getElementById(points[position]);
-    const yOffset = -70;
-    if (!element) {
-      return;
+    const yOffset = window.screen.width < 1024 ? -72 : -56;
+    for (const point of points) {
+      const currentY = window.scrollY;
+      const element = document.getElementById(point);
+      if (!element) {
+        return;
+      }
+      const nextY = element.getBoundingClientRect().top + window.scrollY + yOffset;
+      if (nextY > currentY - yOffset) {
+        window.scrollTo({ top: nextY, behavior: 'smooth' });
+        return;
+      }
     }
-    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-    window.scrollTo({ top: y, behavior: 'smooth' });
-    nextPosition();
   };
 
   return (
@@ -28,7 +22,9 @@ export const JumpButton = ({ points }: { points: string[] }) => {
       data-twe-ripple-color="light"
       className={`!fixed end-6 top-24 z-20 animate-bounce overflow-hidden rounded-full bg-p2-orange p-3 text-xs font-medium uppercase leading-tight text-white shadow-md duration-1000 hover:bg-p2-red hover:shadow-lg focus:bg-p2-red focus:shadow-lg focus:outline-none focus:ring-0 active:bg-p2-red active:shadow-lg`}
       id="btn-back-to-top"
-      onClick={() => jump()}
+      onClick={() => {
+        jump();
+      }}
     >
       <span className="[&>svg]:w-4">
         <svg
