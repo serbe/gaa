cac18825cb8180238782b49bb5e0de7d6382790c
Serbe <serbenv@gmail.com>
2024-11-29 12:18:10 +0300

    new store provider

---
+++ src/providers/gaa-store-provider.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { type ReactNode, createContext, useState, useContext } from 'react';
+import { useStore } from 'zustand';
+
+import { createGaaStore } from '@/stores/gaa-store';
+
+type GaaStoreType = ReturnType<typeof createGaaStore>;
+
+export const GaaStoreContext = createContext<GaaStoreType | null>(null);
+
+export interface GaaStoreProviderProps {
+  children: ReactNode;
+}
+
+export const GaaStoreProvider = ({ children }: GaaStoreProviderProps) => {
+  const [gaaStore] = useState(() => createGaaStore());
+
+  return <GaaStoreContext.Provider value={gaaStore}>{children}</GaaStoreContext.Provider>;
+};
+
+export const useGaaStore = <T,>(
+  selector: (store: ReturnType<GaaStoreType['getState']>) => T,
+): [T, GaaStoreType] => {
+  const gaaStore = useContext(GaaStoreContext);
+
+  if (!gaaStore) {
+    throw new Error(`useGaaStore must be used within GaaStoreProvider`);
+  }
+
+  return [useStore(gaaStore, selector), gaaStore];
+};
--- src/stores/gaa-store.ts
+++ src/stores/gaa-store.ts
@@ -1,14 +1,16 @@
-import { persist } from 'zustand/middleware';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { createStore } from 'zustand/vanilla';
 
 export type GaaState = {
   license: number;
   lider: number;
+  lad: number;
 };
 
 export type GaaActions = {
   setLicense: (licenseNumber: number) => void;
   setLider: (liderNumber: number) => void;
+  setLad: (ladNumber: number) => void;
 };
 
 export type GaaStore = GaaState & GaaActions;
@@ -16,6 +18,7 @@ export type GaaStore = GaaState & GaaActions;
 export const defaultInitState: GaaState = {
   license: 0,
   lider: 0,
+  lad: 0,
 };
 
 export const createGaaStore = (initState: GaaState = defaultInitState) => {
@@ -27,8 +30,14 @@ export const createGaaStore = (initState: GaaState = defaultInitState) => {
           set({ license: licenseNumber >= 0 && licenseNumber <= 2 ? licenseNumber : 0 }),
         setLider: (liderNumber: number) =>
           set({ lider: liderNumber >= 0 && liderNumber <= 2 ? liderNumber : 0 }),
+        setLad: (ladNumber: number) =>
+          set({ lad: ladNumber >= 0 && ladNumber <= 2 ? ladNumber : 0 }),
       }),
-      { name: 'gaa-storage' },
+      {
+        name: 'gaa-store',
+        storage: createJSONStorage(() => localStorage),
+        skipHydration: true,
+      },
     ),
   );
 };
