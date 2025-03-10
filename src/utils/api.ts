bd78b90a8fd2828a816dd35038032d97318a2792
Serbe <serbenv@gmail.com>
2025-03-10 14:22:31 +0300

    add url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,6 @@
+const url = process.env.API_URL;
+//  === 'development' ? 'http://localhost:7070/api' : '/api';
+
 export const fetcher = async (url: string) => {
   const res = await fetch(url, {
     headers: {
@@ -14,7 +17,7 @@ export const fetcher = async (url: string) => {
 };
 
 export const sendRegister = async (username: string, password: string) => {
-  const res = await fetch('http://localhost:8000/api/register', {
+  const res = await fetch(`${url}/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
@@ -24,7 +27,7 @@ export const sendRegister = async (username: string, password: string) => {
 };
 
 export const sendLogin = async (username: string, password: string) => {
-  const res = await fetch('http://localhost:8000/api/login', {
+  const res = await fetch(`${url}/api/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
@@ -34,7 +37,7 @@ export const sendLogin = async (username: string, password: string) => {
 };
 
 export const fetchProtectedData = async (token: string) => {
-  const res = await fetch('http://localhost:8000/api/protected', {
+  const res = await fetch(`${url}/api/protected`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
